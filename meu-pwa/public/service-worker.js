3f3fb2230721f39f3d4b71871ab74a34ea40c21e
JoaoVSantana <joao10victorsantana@gmail.com>
2024-02-16 21:58:49 -0300

    Atualização 16/02

--- meu-pwa/public/service-worker.js
+++ meu-pwa/public/service-worker.js
@@ -1,7 +1,63 @@
+let cachePwa = "cachePwa-v1"
+
+let assets = [
+  "/",
+  "/192x192.png",
+  "/512x512.png",
+  "/manifest.json",
+  "/service-worker.js",
+  "/src/components/index.jsx",
+  "/src/components/ListItem.css",
+  "/src/App.css",
+  "/src/App.jsx",
+  "/src/index.jsx",
+  "/src/main.jsx",
+  "/.eslintrc.cjs",
+  "/index.html",
+  "/package-lock.json",
+  "/package.json",
+  "/vite.config.js",
+]
+
 self.addEventListener('install', event => {
   console.log('Service Worker installing.');
+  event.waitUntil(
+
+    caches.open(cachePwa).then(function (cache) {
+
+      return cache.addAll(assets);
+
+    })
+
+  )
 });
 
-self.addEventListener('activate', event => {
-  console.log('Service Worker activating.');
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys
+        .filter(function (key) {
+          return key.indexOf(cachePwa) !== 0;
+        })
+        .map(function (key) {
+          return caches.delete(key);
+        })
+      );
+    })
+  );
+});
+
+
+self.addEventListener('fetch', function (event) {
+
+  event.respondWith(
+
+    caches.match(event.request).then(function (cachedResponse) {
+
+      return cachedResponse || fetch(event.request);
+
+    })
+
+  );
+
 });
